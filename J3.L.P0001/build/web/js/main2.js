437553b45d4ce1383a9ed2883de5e3a948bd81cc
dinhduc2550 <65802655+dinhduc2550@users.noreply.github.com>
2021-07-12 13:25:50 +0700

    Delete J3.L.P0001 directory

--- J3.L.P0001/build/web/js/main2.js
+++
@@ -1,9 +0,0 @@
-function checkNumbers() {
-    var number = document.getElementById('numbers').value;
-    console.log(number);
-    if (isNaN(number)) {
-        document.getElementById('numbers').value = '';
-        alert('Please Enter A Number!');
-    }
-    
-}
